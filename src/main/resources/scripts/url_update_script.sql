ff9f1b50e42ba43f945d7beaa864f9b20c31e9b8
Arvind <arvind.saravanakumar@gmail.com>
2021-09-29 16:46:27 +0530

    url updates to local

--- src/main/resources/scripts/url_update_script.sql
+++ src/main/resources/scripts/url_update_script.sql
@@ -1,1 +1,4 @@
-select elements.choices_by_url from adapt_dev.elements where elements.choices_by_url is not null;
+update adaptdb.elements set choices_by_url = 'http://localhost:8080/api/adapt/questionnaire/cerealList' where elements_id in (143, 144, 147, 148);
+update adaptdb.elements set choices_by_url = 'http://localhost:8080/api/adapt/questionnaire/medicine' where elements_id in (86, 92, 98, 104, 110, 116);
+update adaptdb.elements set image_link = 'assets/excercise_v_2.png' where elements_id = 198;
+commit;
